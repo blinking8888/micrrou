62c23fb32b4d094f6b6533378608e0339e4c2580
s2R <2113804+n3s2r@users.noreply.github.com>
2023-04-23 10:12:33 +0800

    macrrou: Add docs

--- macrrou/src/lib.rs
+++ macrrou/src/lib.rs
@@ -1,3 +1,9 @@
+//! macrrou
+//! Convenience procedural macro to easily launch an app in the [nannou](https://nannou.cc) framework
+
+#![warn(missing_docs)]
+#![deny(rustdoc::broken_intra_doc_links)]
+
 mod launch_nannou_app;
 
 use proc_macro::TokenStream;
@@ -5,6 +11,7 @@ use syn::parse_macro_input;
 
 use crate::launch_nannou_app::NannouApp;
 
+/// Launches a nannou app
 #[proc_macro]
 pub fn launch_nannou_app(input: TokenStream) -> TokenStream {
     let nannou_app = parse_macro_input!(input as NannouApp);
